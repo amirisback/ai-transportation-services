c6ddde6fb962c90620ea628f313e2cb5c6ab5bc6
amirisback <faisalamircs@gmail.com>
2019-11-26 21:53:27 +0700

    create: Architecture Program

--- src/com/frogobox/Main.java
+++ src/com/frogobox/Main.java
@@ -1,8 +1,27 @@
 package com.frogobox;
 
+import java.util.Scanner;
+
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
+    private void teamView(){
+        System.out.println("Muhammad Faisal Amir (1301198497)");
     }
+
+    private void introView(){
+        System.out.println("Program Transportation");
+    }
+
+    public static void main(String[] args) {
+        // Write your code here
+        new Main().teamView();
+        new Main().introView();
+        new Main().onCreate();
+    }
+
+    private void onCreate(){
+        // Call all function here
+
+    }
+
 }
